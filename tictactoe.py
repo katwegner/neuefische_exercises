e613421de3a687e47876c918c743e9e714e65f2e
katwegner <kwegner2102@gmail.com>
2022-03-24 12:50:21 +0100

    game as class, contains errors

--- tictactoe.py
+++ tictactoe.py
@@ -1,97 +1,93 @@
-
-# create empty 2d array
-def create_board():
-    board = []
-    for i in range(3):
-        row = []
-        for j in range(3):
-            row.append('-')
-        board.append(row)
-    return board
-
-def check_if_full(board):
-    for row in board:
-        for column in row:
-            if column == '-': # if there is at least one empty field
-                return False
-    return True
+class tictactoe:
+    def __init__(self):
+        self.board = []
+        self.player = 1
+        self.field = []
+        self.winner = 0
+        self.icons = ['X', 'O']
 
 
-# Write a function to check whether a player has won or not.
-def check_winner(board):
-    winning_hlines = [[[i,j] for j in range(3)] for i in range(3)]
-    winning_vlines = [[[j,i] for j in range(3)] for i in range(3)]
-    winning_dlines = [[[i,i] for i in range(3)], [[i,2-i] for i in range(3)]]
-    winning_lines = winning_hlines + winning_vlines + winning_dlines
-
-    for w in winning_lines:
-        line = []
-        for field in w:
-            item = board[field[0]][field[1]]
-            if item != '-':
-                line.append(item)
-            if (len(line) == 3) and (len(set(line)) == 1):
-                return True
-
-def show_board(board):
-    for row in board:
-        for item in row:
-            print(item, end=" ")
-        print()
-
-def move(player):
-    field = input('Player ' + str(player) + ': Choose a field (row column)\n').split(' ')
-    field = [int(i) for i in field]
-    return field
-
-def switch_player(player):
-    return (player - 2)*(-1) + 1
-
-def welcome_message():
-    print("TIC TAC TOE \n To make your move, you'll need to enter a row and column, \n both as a number between 1 and 3. Rows and columns are separated by a space. \n")
+    # create empty 2d array
+    def create_board(self):
+        for i in range(3):
+            row = []
+            for j in range(3):
+                row.append('-')
+            self.board.append(row) # doesn't seem to work
 
 
-def start_game():
-    # random selection of starting sign
+    def show_board(self):
+        for row in self.board:
+            for item in row:
+                print(item, end=" ")
+            print()
 
-    # initiate player
-    icons = ['X', 'O']
-    winner = 0
-    player = 1
+    def check_if_full(self):
+        for row in self.board:
+            for column in row:
+                if column == '-': # if there is at least one empty field
+                    return False
+        return True
 
-    # start the game
-    welcome_message()
-    start = input("Press ENTER to start the game.\n\n\n")
-    if start != '':
+    def check_winner(self):
+        winning_hlines = [[[i,j] for j in range(3)] for i in range(3)]
+        winning_vlines = [[[j,i] for j in range(3)] for i in range(3)]
+        winning_dlines = [[[i,i] for i in range(3)], [[i,2-i] for i in range(3)]]
+        winning_lines = winning_hlines + winning_vlines + winning_dlines
+
+        for w in winning_lines:
+            line = []
+            for field in w:
+                item = self.board[field[0]][field[1]]
+                if item != '-':
+                    line.append(item)
+                if (len(line) == 3) and (len(set(line)) == 1):
+                    return True
+
+    def move(self):
+        self.field = input('Player ' + str(self.player) + ': Choose a field (row column)\n').split(' ')
+        self.field = [int(i) for i in self.field]
+
+
+    def switch_player(self):
+        self.player = (self.player - 2)*(-1) + 1
+
+    def welcome_message(self):
+        print("TIC TAC TOE \n To make your move, you'll need to enter a row and column, \n both as a number between 1 and 3. Rows and columns are separated by a space. \n")
+
+    def start_game(self):
+        self.welcome_message()
         start = input("Press ENTER to start the game.\n\n\n")
-    else:
-        board = create_board()
-        show_board(board)
+        if start != '':
+            start = input("Press ENTER to start the game.\n\n\n")
+        else:
+            self.board = self.create_board() # board is not created
+            self.show_board()
+        # play
+        while self.winner == 0:
+            player_icon = self.icons[self.player-1]
+            validity_check = 0
+            while validity_check == 0:
+                self.field = self.move(self.player)
+                if self.board[self.field[0]-1][self.field[1]-1]  == '-': # if valid
+                    validity_check = 1
+                    self.board[self.field[0]-1][self.field[1]-1] = player_icon
+                    self.show_board(self.board)
+                else:
+                    print('Field already taken, try again!')
 
-    # play
-    while winner == 0:
-        player_icon = icons[player-1]
-        validity_check = 0
-        while validity_check == 0:
-            field = move(player)
-            if board[field[0]-1][field[1]-1]  == '-': # if valid
-                validity_check = 1
-                board[field[0]-1][field[1]-1] = player_icon
-                show_board(board)
-            else:
-                print('Field already taken, try again!')
+            # check if there is a winner
+            if self.check_winner(self.board): 
+                self.winner = 1
+                self.show_board(self.board)
+                print("Player " + str(self.player) + " won. Congratulations!")
+            # check if it's full
+            if self.check_if_full(self.board):
+                self.winner = 1
+                self.show_board(self.board)
+                print("It's a tie. Try again.")
+            #switch player
+            self.player = self.switch_player(self.player)
 
-        # check if there is a winner
-        if check_winner(board): 
-            winner = 1
-            show_board(board)
-            print("Player " + str(player) + " won. Congratulations!")
-        # check if it's full
-        if check_if_full(board):
-            winner = 1
-            show_board(board)
-            print("It's a tie. Try again.")
-        #switch player
-        player = switch_player(player)
-
-start_game()
+game = tictactoe()
+game.start_game()
